d74ce79abc9acad0566d5deb408661b52243db45
koba-masa <kobayashi_mshr@koba-masa.com>
2022-02-20 22:18:40 +0900

    refactor: 処理を変更

--- src/models/event.rb
+++ src/models/event.rb
@@ -1,3 +1,5 @@
+# https://docs.aws.amazon.com/ja_jp/ses/latest/dg/receiving-email-action-lambda-event.html
+
 class Event
   def initialize(event)
     @event = event
@@ -13,5 +15,27 @@ class Event
 
   def destination
     @event["Records"][0]["ses"]["mail"]["destination"]
+  end
+
+  def content_transfer_encoding
+    @content_transfer_encoding ||= \
+      headers.select { | header | header["name"] == "Content-Transfer-Encoding" }[0]
+  end
+
+  def content_type_mailtype
+    @content_type_mailtype ||= content_type.split(";")[0].strip
+  end
+
+  def content_type_encoding
+    @content_type_encoding ||= content_type.split(";")[1].strip
+  end
+
+  def content_type
+    @content_transfer_encoding ||= \
+      headers.select { | header | header["name"] == "Content-Typ" }[0]
+  end
+
+  def headers
+    @event["Records"][0]["ses"]["mail"]["headers"]
   end
 end
--- src/s3_mail_transfer.rb
+++ src/s3_mail_transfer.rb
@@ -1,25 +1,84 @@
 require 'json'
 require 'logger'
 
+require 'aws-sdk-ses'
+
+require './models/environment'
 require './models/event'
-require './models/forward_mail'
+require './models/original_mail'
 require './models/alarm_mail'
 
 def lambda_handler(event:, context:)
-  logger = Logger.new($stdout, level: Environment.log_level)
-  logger.debug("Event = #{event}")
+  logger.debug("Event: #{event}")
+  @custom_event = Event.new(event)
 
-  custom_event = Event.new(event)
+  original_mail = OriginalMail.new(custom_event.message_id)
+  logger.debug("Original Raw Email: #{original_mail.body}")
 
-  ForwardMail.new(logger, custom_event).send
+  edited_raw_mail = edit_raw_mail(original_mail)
+  logger.debug("Edited Raw Email: #{edited_raw_mail}")
+
+  send(edited_raw_mail)
+
+  # 想定しているのはtext/plainなメールそれ以外の場合は、念の為、アラームメールを送信し、メッセージ削除を行わない
+  if custom_event.content_type_mailtype == "text/plain"
+    original_mail.delete
+  else
+      error_message = "This mail is not text/plain.: #{custom_event.content_type_mailtype}"
+      logger.warn(error_message)
+      raise error_message
+  end
 
   logger.info("Success")
   { statusCode: 200, body: JSON.generate("Success") }
   rescue => e
-    logger.info("Event = #{event}")
-    logger.info("ErrorMessage = #{e.message}")
+    logger.info("Event: #{event}")
+    logger.info("ErrorMessage: #{e.message}")
     logger.info("#{e.backtrace.join("\n")}")
     logger.error("Failed")
     AlarmMail.new(custom_event, e).send
     { statusCode: 500, body: JSON.generate("Faild") }
+end
+
+def edit_raw_mail(original_mail)
+  original_raw_mail = original_mail.body
+
+  # Return-Path と From の変更
+  # https://docs.aws.amazon.com/ja_jp/ses/latest/dg/request-production-access.html
+  edited_raw_mail = raw_mail \
+    .gsub(/^Return-Path: .+?\r\n/, "Return-Path: #{Environment.transfer_from}\r\n") \
+    .gsub(/\r\nReturn-Path: .+?\r\n/, "Return-Path: #{Environment.transfer_from}\r\n") \
+    .gsub(/^From: .+?\r\n/, "From: #{Environment.transfer_from}\r\n") \
+    .gsub(/\r\nFrom: .+?\r\n/, "\r\nFrom: #{Environment.transfer_from}\r\n")
+  
+
+  add_message = "\r\n== This is the original text. ========\r\nThis mail was transfered.\r\nOriginal From MailAddress: #{event.source}\r\nOriginal To MailAddress: #{event.destination}\r\n" \
+    .encode(custom_event.content_type_encoding)
+
+  edited_raw_mail.concat(add_message)
+end
+
+def send(raw_mail)
+  logger.debug("メッセージ内容:#{raw_mail}")
+  ses_client.send_raw_email(
+    {
+      destinations: Environment.forward_to,
+      source: Environment.transfer_from,
+      raw_message: {
+        data: raw_mail
+      },
+    }
+  )
+end
+
+def custom_event
+  @custom_event
+end
+
+def ses_client
+  @ses_client ||= Aws::SES::Client.new
+end
+
+def logger
+  @logger ||= Logger.new($stdout, level: Environment.log_level)
 end
